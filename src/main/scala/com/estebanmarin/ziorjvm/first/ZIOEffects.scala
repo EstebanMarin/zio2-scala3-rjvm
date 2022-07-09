dea2f40e22ee5e07dbb9fd995edc8a4da990dd62
esteban marin <estebanmarin002@gmail.com>
2022-07-08 21:39:54 -0500

    more

--- src/main/scala/com/estebanmarin/ziorjvm/first/ZIOEffects.scala
+++ src/main/scala/com/estebanmarin/ziorjvm/first/ZIOEffects.scala
@@ -1,3 +1,16 @@
 package com.estebanmarin.ziorjvm.first
 
-object ZIOEffects {}
+import zio.*
+
+val zioEffect: ZIO[Any, Nothing, Int] = ZIO.succeed(42)
+
+def sequenceTakeLast[R, E, A, B](zioa: ZIO[R, E, A], ziob: ZIO[R, E, B]): ZIO[R, E, B] =
+  zioa *> ziob
+def sequenceTakeFirst[R, E, A, B](zioa: ZIO[R, E, A], ziob: ZIO[R, E, B]): ZIO[R, E, A] =
+  zioa <* ziob
+def runForever[R, E, A](zio: ZIO[R, E, A]): ZIO[R, E, A] =
+  zio.forever
+def convert[R, E, A, B](zio: ZIO[R, E, A], value: B): ZIO[R, E, B] =
+  for a <- zio yield value
+def asUnit[R, E, A](zio: ZIO[R, E, A]): ZIO[R, E, Unit] =
+  for a <- zio yield ()
